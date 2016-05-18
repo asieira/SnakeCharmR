150c4a44008b3a4272e3317c0432077fd984585a
Alexandre Sieira <alexandre.sieira@gmail.com>
2016-05-18 12:13:51 -0300

    Merge branch 'master' into i1_check_test
    # Conflicts:
    #	.Rbuildignore
    #	NAMESPACE

---
+++ R/SnakeCharmR.r
@@ -0,0 +1,11 @@
+#' @title R and Python Integration
+#' @author Alexandre Sieira
+#' @description A fork from rPython which uses jsonlite and has several fixes and improvements
+#' @name SnakeCharmR
+#' @docType package
+#' @useDynLib SnakeCharmR
+#' @importFrom Rcpp sourceCpp
+#' @importFrom jsonlite fromJSON toJSON
+#' @importFrom stringr str_replace_all regex str_split
+#' @import utils
+NULL
--- R/py.assign.R
+++ R/py.assign.R
@@ -1,3 +1,30 @@
+#' Assign and get variables in Python from R
+#' 
+#' Functions that assign and get Python variables from R.
+#' 
+#' These functions can assign values to variables in Python as well as get
+#' their values back to R.  Objects are serialized as json strings while being
+#' transferred between R and Python.
+#' 
+#' @param var.name a character string containing a valid python variable name
+#' @param value an R object whose equivalent wants to be assigned to the
+#' variable in python
+#' @param json.opt.args explicit arguments to pass for JSON transformation
+#' @param ...  other arguments passed to the internal \code{toJSON} function
+#' call
+#' @return Function \code{py.get} returns a R version of the Python
+#' variable \code{py.var}.
+#' @references \url{http://code.google.com/p/simplejson}
+#' @keywords manip
+#' @export
+#' @examples
+#' a <- 1:4
+#' py.assign( "a", a )
+#' py.exec( "b = len( a )" )
+#' py.get( "b" )
+#' 
+#' py.exec( "import math" )
+#' py.get( "math.pi" )
 py.assign <- function(var.name, value,
                       json.opt.args = getOption("SnakeCharmR.json.opt.args", list())) {
   # input validation
